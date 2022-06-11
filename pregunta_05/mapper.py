1a42f08e81daac0f30005e45d2e938166480eba7
Carolina Roldán Osorno <101849269+Croldano@users.noreply.github.com>
2022-06-11 11:54:01 -0500

    Pregunta 5
    Intento pregunta 5

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+        
+        divide = row.strip().split("   ")
+        col01 = divide[1]
+        dividefecha = col01.split("-")
+        mes = dividefecha[1]
+     
+        sys.stdout.write(f"{mes}\t1\n")
