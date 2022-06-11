b996e92db3a11d98b0aa1fb0734fef97f3b787d7
Carolina Roldán Osorno <101849269+Croldano@users.noreply.github.com>
2022-06-11 12:41:39 -0500

    Pregunta 8
    Intento pregunta 8

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+
+        divide = row.strip().split("   ")
+        col00 = divide[0]
+        col02 = divide[2]
+  
+    
+        sys.stdout.write(f"{col00}\t{col02}\n")
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from collections import defaultdict
+import operator
+
+if __name__ == '__main__':
+
+    Dict = defaultdict(list)
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+        Dict[key].append(val)
+
+    for clave, lista in Dict.items():
+        sum = round(sum(lista), 1)
+        prom = sum(lista)/len(lista)
+
+        valores = [clave, sum, prom]  
+           
+        sys.stdout.write("{}\t{}\t{}\n".format(valores[0], valores[1], valores[2]))
