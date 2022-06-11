882f348d8327e376fdc2ef38c824d6427b14ba09
Carolina Roldán Osorno <101849269+Croldano@users.noreply.github.com>
2022-06-11 11:36:28 -0500

    Pregunta 03
    Intento pregunta 3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+        
+        Limpiar_linea = row.strip()
+            
+        letra, numero = limpiar_linea.split(",") 
+
+        sys.stdout.write(f"{letra}\t{numero}\n")
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+
+import operator
+import sys
+
+if __name__ == '__main__':
+
+    dict = {}
+    
+    for line in sys.stdin: 
+        key, valor = line.split("\t")
+        valor = int(valor)
+        dict[key] = valor
+          
+    sortedDict = sorted(dict.items(), key=operator.itemgetter(1))
+    for linea in sortedDict:
+            
+        sys.stdout.write("{},{}\n".format(linea[0], linea[1]))
