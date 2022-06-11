5b69eb35d80dc0e8b0cb22dc101098fb732dbd87
Carolina Roldán Osorno <101849269+Croldano@users.noreply.github.com>
2022-06-11 12:32:05 -0500

    Pregunta 07
    Intento pregunta 7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for row in sys.stdin:
+      
+        divide = row.strip().split("   ")
+        col00 = divide[0]
+        col01 = divide[1]
+        col02 = divide[2]
+  
+    
+        sys.stdout.write(f"{col00}\t{col01}\t{col02}\n")
