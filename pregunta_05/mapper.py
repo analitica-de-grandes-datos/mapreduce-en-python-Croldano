91f133b25d4f231f40298bab63e54f074cddc9db
Carolina Roldán Osorno <101849269+Croldano@users.noreply.github.com>
2022-06-11 12:21:41 -0500

    Update mapper.py

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -6,13 +6,10 @@ import sys
 if __name__ == "__main__":
 
     for row in sys.stdin:
-        
+
         divide = row.strip().split("   ")
-        col01 = divide[1]
-        dividefecha = col01.split("-")
+        col01 = dividir[1]
+        dividefecha= col01.split("-")
         mes = dividefecha[1]
-     
+  
         sys.stdout.write(f"{mes}\t1\n")
-
-
-        import sys
